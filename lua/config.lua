2097b48d1937ca062574e076331b4a5b0a2d0374
bogdanshtatskiy-cpu <bogdan.shtatskiy@gmail.com>
2026-03-19 19:02:54 +0200

    Update config.lua

--- lua/config.lua
+++ lua/config.lua
@@ -8,6 +8,10 @@ config.db_secret = ""
 -- Название валюты
 config.currency_name = "ЭМ"
 
+-- Часовой пояс (смещение от UTC в часах)
+-- Впиши цифру. Например: 2 (для UTC+2), 3 (для UTC+3), -5 (для UTC-5)
+config.timezone = 2
+
 -- РУБИЛЬНИК БАЗЫ ДАННЫХ
 -- true = работает с Firebase и веб-панелью
 -- false = работает полностью оффлайн (только жесткий диск компьютера)
@@ -16,6 +20,7 @@ config.use_database = true
 -- Администраторы (кому доступна скрытая панель)
 config.admins = {
     ["DesOope"] = true
+    ["ник"] = true
 }
 
 return config
