9366127b7ed59cdffa026f8ef5d9bc73b434c08e
bogdanshtatskiy-cpu <bogdan.shtatskiy@gmail.com>
2026-03-19 19:08:16 +0200

    Create firebase-config.js

---
+++ firebase-config.js
@@ -0,0 +1,10 @@
+// firebase-config.js
+const firebaseConfig = {
+    apiKey: "AIzaSyCC468vjicHp5fCkxrxwsHTJQ7cXwdZMOw",
+    authDomain: "me-shop-71468.firebaseapp.com",
+    databaseURL: "https://me-shop-71468-default-rtdb.europe-west1.firebasedatabase.app",
+    projectId: "me-shop-71468",
+    storageBucket: "me-shop-71468.firebasestorage.app",
+    messagingSenderId: "44514804398",
+    appId: "1:44514804398:web:57f5a99080902486b9233f"
+};
