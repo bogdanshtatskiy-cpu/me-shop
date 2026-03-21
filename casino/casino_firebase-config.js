f253b46592ddad25f2e4d5a0315378eb00042bd1
bogdanshtatskiy-cpu <bogdan.shtatskiy@gmail.com>
2026-03-21 17:11:25 +0200

    Add files via upload

---
+++ casino/casino_firebase-config.js
@@ -0,0 +1,10 @@
+// casino_firebase-config.js
+const firebaseConfig = {
+  apiKey: "AIzaSyDRJD-4-HgNOcAANPCAWd3BRWWVlj4RueY",
+  authDomain: "desshop-9753e.firebaseapp.com",
+  databaseURL: "https://desshop-9753e-default-rtdb.europe-west1.firebasedatabase.app",
+  projectId: "desshop-9753e",
+  storageBucket: "desshop-9753e.firebasestorage.app",
+  messagingSenderId: "866385276657",
+  appId: "1:866385276657:web:e5efbce7095cf6b7358350"
+};
